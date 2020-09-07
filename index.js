31fddfe1c405e8e9de18dba769255abb2e3a3253
Vincent Servio <vincentservio@gmail.com>
2020-09-07 18:05:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function mapToNegativize (mapp){
+  return mapp.map(n => n *-1) 
+}
+function mapToNoChange(ne){
+ return ne
+}
+
+function mapToDouble(t){
+  
+  return t.map(t => t * 2)
+}
+
+function mapToSquare(v){
+  return v.map( v => v * v)
+}
+
+function reduceToTotal(sourceArray, startingPoint){
+sourceArray()
+  
+}
